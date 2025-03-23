49bd8f322ab510b314f49e93c7d35f51f6f846de
Fábio Nunes <fabionunesbr@gmail.com>
2025-03-23 12:13:50 -0300

    feat: adiconado banco de dados e dialog pizzas

--- Program.cs
+++ Program.cs
@@ -1,7 +1,17 @@
+using BlazingPizza.Data;
+using BlazingPizza.Services;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+builder.Services.AddHttpClient();
+builder.Services.AddScoped<OrderState>();
+
+// Definido conexão com o banco de dados
+var connectionString = builder.Configuration.GetConnectionString("AcessoAPIConnection");
+builder.Services.AddDbContext<PizzaStoreContext>(opts => opts.UseSqlServer(connectionString));
 
 var app = builder.Build();
 
@@ -16,5 +26,17 @@ app.UseRouting();
 app.MapRazorPages();
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
+app.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
+
+// Initialize the database
+var scopeFactory = app.Services.GetRequiredService<IServiceScopeFactory>();
+using (var scope = scopeFactory.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<PizzaStoreContext>();
+    if (db.Database.EnsureCreated())
+    {
+        SeedData.Initialize(db);
+    }
+}
 
 app.Run();
